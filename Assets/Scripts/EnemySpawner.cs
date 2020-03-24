94bc3cde03f019412cbe1ab4a25e0ea09b2ad330
pawelk98 <kumorowski.p@gmail.com>
2020-03-24 16:46:53 +0100

    naprawione spawnowanie; naprawione błędy android
    lanscape default orientation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -24,12 +24,12 @@ public class EnemySpawner : MonoBehaviour
         wavesTab[1] = new int[] { 15, 4, 4, 0 };
         wavesTab[2] = new int[] { 35, 8, 4, 2 };
         wavesTab[3] = new int[] { 60, 4, 10, 2 };
-        wavesTab[4] = new int[] { 100, 10, 0, 8 };
+        wavesTab[4] = new int[] { 80, 10, 0, 8 };
         wavesTab[5] = new int[] { 150, 6, 10, 10 };
-        wavesTab[6] = new int[] { 200, 0, 20, 0 };
-        wavesTab[7] = new int[] { 250, 10, 10, 10 };
-        wavesTab[8] = new int[] { 320, 15, 5, 20 };
-        wavesTab[9] = new int[] { 400, 20, 20, 20 };
+        wavesTab[6] = new int[] { 170, 0, 20, 0 };
+        wavesTab[7] = new int[] { 200, 10, 10, 10 };
+        wavesTab[8] = new int[] { 230, 15, 5, 20 };
+        wavesTab[9] = new int[] { 270, 20, 20, 20 };
         //sec w ktorej sie zaczyna, ilosc lvl1, ilosc lvl2...
     }
 
@@ -44,7 +44,7 @@ public class EnemySpawner : MonoBehaviour
 
     void spawnWave(int id)
     {
-        for (int i = 1; i < enemies.Length; i++)
+        for (int i = 1; i < wavesTab[i].Length; i++)
         {
             for (int j = 0; j < wavesTab[id][i]; j++)
             {
