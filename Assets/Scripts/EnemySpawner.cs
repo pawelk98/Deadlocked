210fbfcf2fea6cec13f30c1bf645c0a6c887678d
pawelk98 <kumorowski.p@gmail.com>
2020-03-24 16:01:11 +0100

    poprawione spawnowanie przeciwników

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,51 +6,49 @@ public class EnemySpawner : MonoBehaviour
 {
     public Transform[] spawns;
     public GameObject[] enemies;
-    public int wavesNumber = 10;
-    public float wavesRate = 10;
 
     private int[][] wavesTab;
-    private float lastWave;
-    private int wavesCounter = 0;
+    private int wavesNumber;
+    private int actualWave = 0;
+    private float startTime;
 
     void Start()
     {
-        lastWave = Time.time - wavesRate;
+        startTime = Time.time;
 
-        wavesTab = new int[wavesNumber][];
+        wavesTab = new int[10][];
+        wavesNumber = wavesTab.Length;
 
         //ilosci przeciwnikow w kolejnych falach
-        wavesTab[0] = new int[] { 4, 0, 0 };
-        wavesTab[1] = new int[] { 8, 0, 0 };
-        wavesTab[2] = new int[] { 8, 4, 0 };
-        wavesTab[3] = new int[] { 4, 10, 0 };
-        wavesTab[4] = new int[] { 4, 6, 4 };
-        wavesTab[5] = new int[] { 6, 10, 4 };
-        wavesTab[6] = new int[] { 4, 0, 10 };
-        wavesTab[7] = new int[] { 10, 10, 5 };
-        wavesTab[8] = new int[] { 5, 8, 15 };
-        wavesTab[9] = new int[] { 10, 15, 10 };
-        //--------------------------------------
+        wavesTab[0] = new int[] { 0, 4, 0, 0 };
+        wavesTab[1] = new int[] { 15, 4, 4, 0 };
+        wavesTab[2] = new int[] { 35, 8, 4, 2 };
+        wavesTab[3] = new int[] { 60, 4, 10, 2 };
+        wavesTab[4] = new int[] { 100, 10, 0, 8 };
+        wavesTab[5] = new int[] { 150, 6, 10, 10 };
+        wavesTab[6] = new int[] { 200, 0, 20, 0 };
+        wavesTab[7] = new int[] { 250, 10, 10, 10 };
+        wavesTab[8] = new int[] { 320, 15, 5, 20 };
+        wavesTab[9] = new int[] { 400, 20, 20, 20 };
+        //sec w ktorej sie zaczyna, ilosc lvl1, ilosc lvl2...
     }
 
     void Update()
     {
-        if (Time.time - lastWave > wavesRate && wavesCounter <= wavesNumber)
+        if (actualWave < wavesNumber && Time.time > startTime + wavesTab[actualWave][0])
         {
-            spawnWave(wavesCounter);
-
-            lastWave = Time.time;
-            wavesCounter++;
+            spawnWave(actualWave);
+            actualWave++;
         }
     }
 
     void spawnWave(int id)
     {
-        for (int i = 0; i < enemies.Length; i++)
+        for (int i = 1; i < enemies.Length; i++)
         {
             for (int j = 0; j < wavesTab[id][i]; j++)
             {
-                GameObject enemy = Instantiate(enemies[i], spawns[j % spawns.Length].position, Quaternion.identity);
+                GameObject enemy = Instantiate(enemies[i-1], spawns[j % spawns.Length].position, Quaternion.identity);
             }
         }
     }
